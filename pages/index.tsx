ddfef8f60d4406ed04b7f9f47e2af28d97ff957f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-10 03:50:34 +0100

    add user link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,25 @@
 import type { NextPage } from "next";
 import Header from "../components/Header";
+import Link from "next/link";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
     <Header>
-      <div></div>
+      <div className="min-h-screen w-screen p-2 py-6">
+        <nav className="max-w-[320px]">
+          <Link href="/user/id">
+            <a className="text-black flex items-center justify-start gap-3 hover:bg-[#e4e6e9] p-2 rounded-lg">
+              <div className="w-10 h-10 relative rounded-full overflow-hidden">
+                <Image src="https://picsum.photos/600" alt="" layout="fill" />
+              </div>
+              <h2>Username</h2>
+            </a>
+          </Link>
+        </nav>
+        <main></main>
+        <div></div>
+      </div>
     </Header>
   );
 };
