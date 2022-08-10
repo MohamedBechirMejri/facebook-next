e897e3241a9d13c4ec71096fa5342df2a2ba963f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-10 03:54:59 +0100

    add events link

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,6 +72,14 @@ const Home: NextPage = () => {
               <h2>Pages</h2>
             </a>
           </Link>
+          <Link href="/events">
+            <a className="text-black flex items-center justify-start gap-3 hover:bg-[#e4e6e9] p-2 rounded-lg">
+              <div className="w-10 h-10 relative rounded-full overflow-hidden">
+                <Image src="https://picsum.photos/600" alt="" layout="fill" />
+              </div>
+              <h2>Events</h2>
+            </a>
+          </Link>
         </nav>
         <main></main>
         <div></div>
