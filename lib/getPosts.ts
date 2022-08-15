8a7e390e775dcf7c108bc72d7b53fa983e8db672
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-15 07:43:02 +0100

    fix next js bug

--- lib/getPosts.ts
+++ lib/getPosts.ts
@@ -1,10 +1,24 @@
 import Post from "../models/Post";
+import User from "../models/User";
 import dbConnect from "./dbConnect";
 
 const getPosts = async () => {
   await dbConnect();
 
-  return await Post.find({});
+  const result = await Post.find({}).populate(
+    "author",
+    "firstName lastName nickname picture id",
+    User
+  );
+  return result.map(doc => {
+    const post = doc.toObject();
+    post._id = post._id.toString();
+    post.author._id = post.author._id.toString();
+    post.reacts._id = post.reacts._id.toString();
+    post.createdAt = post.createdAt.toISOString();
+    post.updatedAt = post.updatedAt.toISOString();
+    return post;
+  });
 };
 
 export default getPosts;
