7e2ebb0c64b1e118333f417891f572227b4ac614
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-15 01:45:52 +0100

    show group/page name

--- components/Post.tsx
+++ components/Post.tsx
@@ -16,8 +16,12 @@ const Post = ({ post }: { post: PostType }) => {
             </a>
           </Link>
           <div>
-            <h1 className="font-bold">Author Name</h1>
+            <h1 className="font-bold">
+              {post.group && post.group.name} {post.page && post.page.name}{" "}
+              {!post.group && !post.page && post.author.name}
+            </h1>
             <p className="flex items-center gap-1 text-xs">
+              {post.group && <span>{post.author.name} ·</span>}
               <span>8h</span> ·{" "}
               <span>
                 <svg
