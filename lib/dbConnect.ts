26d29b35afabe77883dcbbb7baf287e0f850ab21
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-15 02:21:58 +0100

    rename js to ts

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -5,17 +5,20 @@ const connection = {}; /* creating connection object*/
 
 async function dbConnect() {
   /* check if we have connection to our databse*/
+  // @ts-ignore
   if (connection.isConnected) {
     return;
   }
 
   /* connecting to our database */
+  // @ts-ignore
   const db = await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
   });
 
+  // @ts-ignore
   connection.isConnected = db.connections[0].readyState;
 }
 
