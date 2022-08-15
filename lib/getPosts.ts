e0e7460b3f9f01532368c697e6566eb429c97c73
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-15 07:51:50 +0100

    fix more next js bugs

--- lib/getPosts.ts
+++ lib/getPosts.ts
@@ -17,6 +17,14 @@ const getPosts = async () => {
     post.reacts._id = post.reacts._id.toString();
     post.createdAt = post.createdAt.toISOString();
     post.updatedAt = post.updatedAt.toISOString();
+    post.reacts.likes = post.reacts.likes.map((id: any) => id.toString());
+    post.reacts.loves = post.reacts.loves.map((id: any) => id.toString());
+    post.reacts.hahas = post.reacts.hahas.map((id: any) => id.toString());
+    post.reacts.wows = post.reacts.wows.map((id: any) => id.toString());
+    post.reacts.sads = post.reacts.sads.map((id: any) => id.toString());
+    post.reacts.angrys = post.reacts.angrys.map((id: any) => id.toString());
+    post.shares = post.shares.map((id: any) => id.toString());
+
     return post;
   });
 };
