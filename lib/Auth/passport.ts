ffcc3943874d2c460769ed159fbe79d1035316ce
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-19 20:16:17 +0100

    find or create user

--- lib/Auth/passport.ts
+++ lib/Auth/passport.ts
@@ -1,4 +1,5 @@
 import User from "../../models/User";
+import dbConnect from "../dbConnect";
 
 const passport = require("passport");
 const FacebookStrategy = require("passport-facebook");
@@ -8,11 +9,39 @@ passport.use(
     {
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: "/api/auth/callback",
+      callbackURL: "/auth/callback",
+      profileFields: [
+        "id",
+        "first_name",
+        "picture",
+        "email",
+        "last_name",
+        "middle_name",
+        "birthday",
+        "gender",
+      ],
     },
     //@ts-ignore
-    function (accessToken, refreshToken, profile, cb) {
-      return cb(null, profile);
+    async function (accessToken, refreshToken, profile, cb) {
+      await dbConnect();
+
+      User.findOne({ facebookId: profile.id }, (err: any, user: any) => {
+        if (user) return cb(null, user);
+        else {
+          const newUser = new User({
+            facebookId: profile.id,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            email: profile.emails[0].value,
+            picture: profile.photos[0].value,
+          });
+          if (profile.username) newUser.nickname = profile.username;
+          newUser.save((err: any) => {
+            if (err) return cb(err);
+            return cb(null, newUser);
+          });
+        }
+      });
     }
   )
 );
