af62151cf1243ad1ad1bb5cabb0e5e06a3709841
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-21 06:44:23 +0100

    remove /add route

--- pages/api/posts/add.ts
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-import addPost from "../../../lib/addPost";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  addPost();
-  res.status(200).json("done");
-}
