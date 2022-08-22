0f5cdb2a6740082a956c57751836aa297bcc30d6
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-22 05:10:57 +0100

    add upload image function

---
+++ lib/uploadImage.ts
@@ -0,0 +1,21 @@
+const uploadImage = async (image: any) => {
+  const headers = new Headers();
+  headers.append(
+    "Authorization",
+    `Client-ID ${process.env.NEXT_PUBLIC_IMGUR_CLIENT_ID}`
+  );
+
+  const formdata = new FormData();
+  formdata.append("image", image);
+
+  const requestOptions = {
+    method: "POST",
+    headers: headers,
+    body: formdata,
+  };
+  const response = await fetch("https://api.imgur.com/3/image", requestOptions);
+  const result = await response.json();
+  return result.data.link;
+};
+
+export default uploadImage;
