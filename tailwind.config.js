24609499571f5e7c4c794d92d71d575bd54ea187
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-25 09:08:07 +0100

    add reveal animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    animation: { reveal: "reveal .3s forwards" },
+    keyframes: {
+      reveal: {
+        "0%": {
+          opacity: 0,
+          transform: "translateY(1rem)",
+        },
+        "100%": {
+          opacity: 1,
+          transform: "translateY(0)",
+        },
+      },
+    },
   },
   plugins: [],
 };
