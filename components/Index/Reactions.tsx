6855d28bfe951184bf4f4e1fde4789d8c152c251
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-25 07:51:02 +0100

    tweak  image size

--- components/Index/Reactions.tsx
+++ components/Index/Reactions.tsx
@@ -3,22 +3,22 @@ const Reactions = () => {
   return (
     <div className="absolute flex items-center justify-center gap-2 p-2 bg-white border rounded-full -top-14 left-8">
       <button className="transition-all hover:scale-125 active:scale-100">
-        <Image src="/Assets/rlike.png" height={39} width={39} alt="like" />
+        <Image src="/Assets/rlike.png" height={35} width={35} alt="like" />
       </button>
       <button className="transition-all hover:scale-125 active:scale-100">
-        <Image src="/Assets/rlove.png" height={39} width={39} alt="love" />
+        <Image src="/Assets/rlove.png" height={35} width={35} alt="love" />
       </button>
       <button className="transition-all hover:scale-125 active:scale-100">
-        <Image src="/Assets/rhaha.png" height={39} width={39} alt="haha" />
+        <Image src="/Assets/rhaha.png" height={35} width={35} alt="haha" />
       </button>
       <button className="transition-all hover:scale-125 active:scale-100">
-        <Image src="/Assets/rwow.png" height={39} width={39} alt="wow" />
+        <Image src="/Assets/rwow.png" height={35} width={35} alt="wow" />
       </button>
       <button className="transition-all hover:scale-125 active:scale-100">
-        <Image src="/Assets/rsad.png" height={39} width={39} alt="sad" />
+        <Image src="/Assets/rsad.png" height={35} width={35} alt="sad" />
       </button>
       <button className="transition-all hover:scale-125 active:scale-100">
-        <Image src="/Assets/rangry.png" height={39} width={39} alt="angry" />
+        <Image src="/Assets/rangry.png" height={35} width={35} alt="angry" />
       </button>
     </div>
   );
