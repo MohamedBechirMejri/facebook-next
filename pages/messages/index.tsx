a0147679dd04ef3ea7762ab8f0dd5a8ed71f8d60
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-26 16:22:56 +0100

    add placeholders

--- pages/messages/index.tsx
+++ pages/messages/index.tsx
@@ -5,7 +5,16 @@ import type { NextApiResponse, NextApiRequest } from "next";
 const Messages = ({ user }: { user: any }) => {
   return (
     <Header user={user}>
-      <div className="relative flex w-screen p-2 text-black bg-black h-[93vh]"></div>
+      <div className="relative flex w-screen p-2 text-black h-[93vh]  ">
+        {/* SECTION: Contacts   */}
+        <div className="w-[360px] h-full bg-red-400 shrink-0"></div>
+        <div className="flex w-full h-full bg-blue-400">
+          {/* SECTION: Main   */}
+          <main className="w-full h-full bg-blue-400"></main>
+          {/* SECTION: Info   */}
+          <div className="w-[360px] h-full bg-green-400 shrink-0"></div>
+        </div>
+      </div>
     </Header>
   );
 };
