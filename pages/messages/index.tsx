ae52485b466f370e5044d2c70ed4a5a9f1f79324
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-27 08:33:40 +0100

    tweak serch bar

--- pages/messages/index.tsx
+++ pages/messages/index.tsx
@@ -71,14 +71,14 @@ const Messages = ({ user }: { user: any }) => {
           <h1 className="pb-4 text-2xl font-semibold">
             <span>Chats</span>
           </h1>
-          <div className="rounded-full bg-[#f0f2f5] flex items-center justify-left p-1 px-2 transition-all">
+          <div className="rounded-full bg-[#f0f2f5] flex items-center justify-left p-2 transition-all">
             <div className="relative w-8 h-4">
               <Image src="/Assets/search.svg" layout="fill" alt="" />
             </div>
             <input
               type="text"
               className="bg-transparent placeholder:text-[#656797] outline-none text-black transition-all"
-              placeholder="Search Facebook"
+              placeholder="Search Messenger"
             />
           </div>
         </div>
