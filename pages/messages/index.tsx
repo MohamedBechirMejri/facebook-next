955e86155f727c1d89bfc9f1a50f2b80d42f2475
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-08-27 10:00:51 +0100

    put info in own component

--- pages/messages/index.tsx
+++ pages/messages/index.tsx
@@ -3,7 +3,7 @@ import getUser from "../../lib/Auth/getUser";
 import type { NextApiResponse, NextApiRequest } from "next";
 import Image from "next/image";
 import { useState } from "react";
-import Link from "next/link";
+import Info from "../../components/Messages/Info";
 
 const Messages = ({ user }: { user: any }) => {
   const [messages, setMessages] = useState([
@@ -322,63 +322,7 @@ const Messages = ({ user }: { user: any }) => {
               <button className="text-xl">👍🏻</button>
             </div>
           </main>
-          {/* SECTION: Info   */}
-          <div className="w-[360px] h-full shrink-0 flex p-2 items-center flex-col">
-            <div className="w-24 shrink-0">
-              <Image
-                src={"https://picsum.photos/900"}
-                width={1000}
-                height={1000}
-                alt="user"
-                style={{
-                  borderRadius: "100%",
-                }}
-              />
-            </div>
-            <p className="font-bold">Name</p>
-            <div className="flex flex-col items-center p-4">
-              <Link href={"/"}>
-                <a className="p-[.2rem] px-2 pt-2 bg-gray-200 rounded-full hover:bg-gray-300 transition-all">
-                  <div
-                    style={{
-                      backgroundImage: `url(${"/Assets/buttons7.png"})`,
-                      backgroundPosition: "0px -149px",
-                    }}
-                    className="w-[20px] h-[20px] bg-no-repeat inline-block bg-auto"
-                  />
-                </a>
-              </Link>
-              <p className="text-sm">Profile</p>
-            </div>
-            <div className="w-full text-left">
-              <button className="flex items-center justify-between w-full p-4 font-bold transition-all hover:bg-gray-200 rounded-xl active:bg-gray-300">
-                <span> Customize chat </span>
-
-                <div
-                  style={{
-                    backgroundImage: `url(${"/Assets/buttons8.png"})`,
-                    backgroundPosition: "-168px -111px",
-                  }}
-                  className="w-[20px] h-[20px] bg-no-repeat inline-block bg-auto"
-                />
-              </button>
-              <button className="flex items-center justify-start w-full gap-4 p-4 py-2 transition-all hover:bg-gray-200 rounded-xl active:bg-gray-300">
-                <div
-                  style={{
-                    backgroundColor: `#123`,
-                  }}
-                  className="w-[20px] h-[20px] rounded-full"
-                />
-                <span> Change theme</span>
-              </button>
-              <button className="flex items-center justify-start w-full gap-6 p-4 py-2 transition-all hover:bg-gray-200 rounded-xl active:bg-gray-300">
-                <div className="w-[20px] h-[20px] text-xl leading-none -m-1">
-                  👍🏻
-                </div>
-                <span>Change Emoji</span>
-              </button>
-            </div>
-          </div>
+          <Info />
         </div>
       </div>
     </Header>
