dcb538a2ccaff3288f18f0ba1690b14f668d3e8c
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-09-02 16:35:17 +0100

    extract video svg

--- components/Messages/Main.tsx
+++ components/Messages/Main.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import CallSvg from "./SVGs/Call";
+import VideoSvg from "./SVGs/Video";
 
 const Main = ({
   user,
@@ -90,17 +91,7 @@ const Main = ({
             <CallSvg />
           </button>
           <button className="p-1 transition-all rounded-full hover:bg-gray-200 active:bg-gray-300">
-            <svg
-              role="presentation"
-              width="34px"
-              height="34px"
-              viewBox="-5 -5 30 30"
-            >
-              <path
-                d="M19.492 4.112a.972.972 0 00-1.01.063l-3.052 2.12a.998.998 0 00-.43.822v5.766a1 1 0 00.43.823l3.051 2.12a.978.978 0 001.011.063.936.936 0 00.508-.829V4.94a.936.936 0 00-.508-.828zM10.996 18A3.008 3.008 0 0014 14.996V5.004A3.008 3.008 0 0010.996 2H3.004A3.008 3.008 0 000 5.004v9.992A3.008 3.008 0 003.004 18h7.992z"
-                fill="#005fff"
-              ></path>
-            </svg>
+            <VideoSvg />
           </button>
 
           <button
