ebb4677bc36e254f72d113c2cb603a86747bff17
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-09-02 16:40:01 +0100

    extract media svg

--- components/Messages/Main.tsx
+++ components/Messages/Main.tsx
@@ -4,6 +4,7 @@ import CallSvg from "./SVGs/Call";
 import VideoSvg from "./SVGs/Video";
 import InfoSvg from "./SVGs/Info";
 import MoreSvg from "./SVGs/More";
+import MediaSvg from "./SVGs/Media";
 
 const Main = ({
   user,
@@ -178,25 +179,7 @@ const Main = ({
             <MoreSvg />
           </button>{" "}
           <button className="p-1 transition-all rounded-full hover:bg-gray-200 active:bg-gray-300">
-            <svg viewBox="0 -1 17 17" height="20px" width="20px">
-              <g fill="none" fillRule="evenodd">
-                <path
-                  d="M2.882 13.13C3.476 4.743 3.773.48 3.773.348L2.195.516c-.7.1-1.478.647-1.478 1.647l1.092 11.419c0 .5.2.9.4 1.3.4.2.7.4.9.4h.4c-.6-.6-.727-.951-.627-2.151z"
-                  fill="#005cf8"
-                ></path>
-                <circle fill="#005cf8" cx="8.5" cy="4.5" r="1.5"></circle>
-                <path
-                  d="M14 6.2c-.2-.2-.6-.3-.8-.1l-2.8 2.4c-.2.1-.2.4 0 .6l.6.7c.2.2.2.6-.1.8-.1.1-.2.1-.4.1s-.3-.1-.4-.2L8.3 8.3c-.2-.2-.6-.3-.8-.1l-2.6 2-.4 3.1c0 .5.2 1.6.7 1.7l8.8.6c.2 0 .5 0 .7-.2.2-.2.5-.7.6-.9l.6-5.9L14 6.2z"
-                  fill="#005cf8"
-                ></path>
-                <path
-                  d="M13.9 15.5l-8.2-.7c-.7-.1-1.3-.8-1.3-1.6l1-11.4C5.5 1 6.2.5 7 .5l8.2.7c.8.1 1.3.8 1.3 1.6l-1 11.4c-.1.8-.8 1.4-1.6 1.3z"
-                  stroke="#005cf8"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                ></path>
-              </g>
-            </svg>
+            <MediaSvg />
           </button>{" "}
           <button className="p-1 transition-all rounded-full hover:bg-gray-200 active:bg-gray-300">
             <svg x="0px" y="0px" viewBox="0 0 17 16" height="20px" width="20px">
