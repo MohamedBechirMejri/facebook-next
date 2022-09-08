9fac1459e76fc24b2c4466b1629560fe2a2a461e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-09-08 23:57:26 +0100

    show gifs list

--- components/Messages/GifOverlay.tsx
+++ components/Messages/GifOverlay.tsx
@@ -1,19 +1,17 @@
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import GifSvg from "./SVGs/Gif";
 const giphy = require("giphy-api")(process.env.NEXT_PUBLIC_GIPHY_API_KEY);
 
 const GifOverlay = ({ theme }: { theme: string }) => {
   const [isVisible, setIsVisible] = useState(false);
+  const [gifs, setGifs] = useState([]);
 
-  //   useEffect(() => {
-  //     giphy.search("pokemon").then((res: any) => {
-  //     //   console.log("res: ", res.data);
-  //         res.data.forEach(gif => {
-  //             console.log(gif.images.original.url);
-  //
-  //         })
-  //     });
-  //   }, []);
+  useEffect(() => {
+    giphy.search("pokemon").then((res: any) => {
+      setGifs(res.data);
+    });
+  }, []);
 
   return (
     <div className="flex items-center h-full">
@@ -25,7 +23,32 @@ const GifOverlay = ({ theme }: { theme: string }) => {
       </button>
 
       {isVisible && (
-        <div className="absolute z-20 flex flex-col bg-white border rounded-lg shadow w-max right-16 h-max "></div>
+        <div className="absolute z-20 flex flex-col overflow-scroll bg-white border rounded-lg shadow -top-[720%] w-[20rem] -left-0 h-[25rem]">
+          <div className="sticky top-0 z-10 w-full p-2 px-4 bg-white">
+            <input
+              type="search"
+              className="w-full h-10 bg-[#f0f2f5] rounded-full outline-none px-4"
+              placeholder="Search"
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            {gifs.map((gif: any, i: number) => {
+              return (
+                <div
+                  key={i}
+                  className="m-1 overflow-hidden transition-all rounded-lg shadow cursor-pointer active:scale-95 w-fit"
+                >
+                  <Image
+                    src={gif.images.original.url}
+                    height={gif.images.original.height}
+                    width={gif.images.original.width}
+                    alt=""
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
       )}
       {isVisible && (
         <div
