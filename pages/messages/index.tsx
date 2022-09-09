d4affc90d39cb64ed9d9966a7588a6657e713fab
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-09-09 02:10:52 +0100

    fix bug

--- pages/messages/index.tsx
+++ pages/messages/index.tsx
@@ -8,17 +8,16 @@ import { useEffect } from "react";
 
 const Messages = ({ user }: { user: any }) => {
   const router = useRouter();
+  const conversations = user.conversations.map((c: any) => JSON.parse(c));
 
   useEffect(() => {
-    const conversations = user.conversations.map((c: any) => JSON.parse(c));
-
     if (conversations[0]) router.push("/messages/" + conversations[0]._id);
-  }, [router, user.conversations]);
+  }, [conversations, router]);
 
   return (
     <Header user={user}>
       <div className="relative flex w-screen text-black h-[93.45vh] bg-white ">
-        <Chats />
+        <Chats user={user} conversations={conversations} />
         <div className="flex w-full h-full ">
           <main className="flex flex-col items-center justify-center w-full h-full font-semibold">
             {true ? (
