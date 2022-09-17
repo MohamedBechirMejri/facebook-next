33c2fe115e97317e25e90da8aeb4299a60262441
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-09-17 04:45:25 +0100

    add mobile design

--- pages/messages/index.tsx
+++ pages/messages/index.tsx
@@ -5,6 +5,8 @@ import Chats from "../../components/Messages/Chats";
 import { L49 } from "react-isloading";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import FriendsOverlay from "../../components/Messages/FriendsOverlayMobile";
+import Image from "next/image";
 
 const Messages = ({ user }: { user: any }) => {
   const router = useRouter();
@@ -17,10 +19,45 @@ const Messages = ({ user }: { user: any }) => {
   return (
     <Header user={user}>
       <div className="relative flex w-screen text-black h-[93.45vh] bg-white ">
-        <Chats user={user} conversations={conversations} />
-        <div className="flex w-full h-full ">
+        {/* <Chats
+          user={user}
+          conversations={conversations}
+          className="w-full h-full px-4 py-2 shrink-0 xl:block"
+        /> */}
+        <div className="flex flex-col w-full p-4 overflow-scroll overflow-y-scroll bg-white rounded-lg xl:w-[400px] ">
+          <h1 className="w-full text-xl font-semibold text-center">
+            Start a New Chat
+          </h1>
+          {user.friends.map((friend: any) => (
+            <div
+              key={friend._id}
+              className="flex items-center justify-between py-4 text-lg "
+            >
+              <Image
+                src={friend.picture}
+                height={50}
+                width={50}
+                alt=""
+                style={{
+                  borderRadius: "9999999px",
+                }}
+              />
+              <h1>
+                {friend.firstName} {friend.lastName}
+              </h1>
+              <a
+                href={"/api/users/" + friend._id + "/message"}
+                className="p-1 px-2 text-base text-white transition-all bg-blue-600 rounded-lg hover:bg-blue-700 active:scale-95"
+              >
+                Message
+              </a>
+            </div>
+          ))}
+        </div>
+
+        <div className="hidden w-full h-full xl:flex">
           <main className="flex flex-col items-center justify-center w-full h-full font-semibold">
-            {true ? (
+            {false ? (
               <L49
                 style={{
                   height: "7rem",
