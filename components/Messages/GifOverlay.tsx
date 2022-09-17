d14abcd88be5f6c6a03e7acdde53c329878f1813
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-09-17 05:48:32 +0100

    fix width

--- components/Messages/GifOverlay.tsx
+++ components/Messages/GifOverlay.tsx
@@ -42,7 +42,7 @@ const GifOverlay = ({
       </button>
 
       {isVisible && (
-        <div className="absolute z-20 flex flex-col overflow-scroll bg-white border rounded-lg shadow -top-[720%] w-[20rem] -left-0 h-[25rem]">
+        <div className="absolute z-20 flex flex-col overflow-scroll bg-white border rounded-lg shadow -top-[720%] w-full xl:w-[20rem] -left-0 h-[25rem]">
           <div className="sticky top-0 z-10 w-full p-2 px-4 bg-white">
             <input
               type="search"
