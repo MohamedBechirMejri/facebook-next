bbd90e8db6402b26baa22c0c5dbfc4bbaea72548
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-11-28 17:32:23 +0100

    allow all ing domains

--- next.config.js
+++ next.config.js
@@ -2,22 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    domains: [
-      "picsum.photos",
-      "www.gravatar.com",
-      "platform-lookaside.fbsbx.com",
-      "firebasestorage.googleapis.com",
-      "scontent.ftun9-1.fna.fbcdn.net",
-      "media7.giphy.com",
-      "media6.giphy.com",
-      "media5.giphy.com",
-      "media4.giphy.com",
-      "media3.giphy.com",
-      "media2.giphy.com",
-      "media1.giphy.com",
-      "media0.giphy.com",
-      "pbs.twimg.com",
+   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
     ],
   },
 };
