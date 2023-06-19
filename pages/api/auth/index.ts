e7037a599f4a995651e38c9d223ab2429e5874ce
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-06-19 09:35:45 +0100

    update login method

--- pages/api/auth/index.ts
+++
@@ -1,8 +0,0 @@
-import passport from "../../../lib/Auth/passport";
-
-export default passport.authenticate("facebook", {
-  scope: [
-    // "email",
-    "public_profile",
-  ],
-});
--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-const jwt = require("jsonwebtoken");
+
+import jwt from "jsonwebtoken";
 import { setCookie } from "cookies-next";
 import dbConnect from "../../../lib/dbConnect";
 import User from "../../../models/User";
@@ -7,20 +8,17 @@ import User from "../../../models/User";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   await dbConnect();
 
-  const dummyAccounts = [
-    "63256014f346205125e987c4",
-    "63256070f346205125e987c5",
-  ];
+  const { email, password } = req.body;
 
-  const guestId =
-    dummyAccounts[Math.floor(Math.random() * dummyAccounts.length)];
+  const user = await User.findOne({ email, password });
 
-  const guest = await User.findById(guestId);
+  if (!user) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
 
   jwt.sign(
-    // @ts-ignore
-    { user: guest },
-    process.env.JWT_SECRET,
+    { user },
+    process.env.JWT_SECRET!,
     { expiresIn: "15d" },
     (err: any, token: any) => {
       setCookie("token", token, { req, res, maxAge: 60 * 60 * 24 * 15 });
