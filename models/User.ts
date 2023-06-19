f85450998b67aef3ab9e216becd03d76e40fe930
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-06-19 11:29:30 +0100

    add password field

--- models/User.ts
+++ models/User.ts
@@ -2,93 +2,25 @@ import mongoose, { Schema, model } from "mongoose";
 
 const UserSchema = new Schema(
   {
-    facebookId: { type: String },
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    // email: {
-    //   type: String,
-    //   required: true,
-    //   unique: true,
-    // },
-    // nickname: {
-    //   type: String,
-    //   unique: true,
-    // },
-    picture: {
-      type: String,
-    },
-    friends: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    posts: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Post",
-      },
-    ],
-    groups: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Group",
-      },
-    ],
-    pages: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Page",
-      },
-    ],
-    conversations: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Conversation",
-      },
-    ],
-    blocks: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    saves: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Post",
-      },
-    ],
-    notifications: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Notification",
-      },
-    ],
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { required: true, type: String },
+    picture: { type: String },
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    posts: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+    groups: [{ type: Schema.Types.ObjectId, ref: "Group" }],
+    pages: [{ type: Schema.Types.ObjectId, ref: "Page" }],
+    conversations: [{ type: Schema.Types.ObjectId, ref: "Conversation" }],
+    blocks: [{ type: Schema.Types.ObjectId, ref: "User" }],
+    saves: [{ type: Schema.Types.ObjectId, ref: "Post" }],
+    notifications: [{ type: Schema.Types.ObjectId, ref: "Notification" }],
     friendRequests: {
-      received: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: "User",
-        },
-      ],
-      sent: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: "User",
-        },
-      ],
+      received: [{ type: Schema.Types.ObjectId, ref: "User" }],
+      sent: [{ type: Schema.Types.ObjectId, ref: "User" }],
     },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
 export default mongoose.models.User || model("User", UserSchema);
