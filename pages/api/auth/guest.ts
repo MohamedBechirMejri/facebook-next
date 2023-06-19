72c90bbbf570ffab86bd9159b3195d0c16f0a220
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2023-06-19 11:39:55 +0100

    restore guest route

---
+++ pages/api/auth/guest.ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import jwt from "jsonwebtoken";
+import { setCookie } from "cookies-next";
+
+import dbConnect from "~/lib/dbConnect";
+import User from "~/models/User";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  await dbConnect();
+
+  const dummyAccounts = [
+    "63256014f346205125e987c4",
+    "63256070f346205125e987c5",
+  ];
+
+  const guestId =
+    dummyAccounts[Math.floor(Math.random() * dummyAccounts.length)];
+
+  const guest = await User.findById(guestId);
+
+  jwt.sign(
+    // @ts-ignore
+    { user: guest },
+    process.env.JWT_SECRET!,
+    { expiresIn: "15d" },
+    (err: any, token: any) => {
+      setCookie("token", token, { req, res, maxAge: 60 * 60 * 24 * 15 });
+      res.redirect("/");
+    }
+  );
+};
+
+export default handler;
